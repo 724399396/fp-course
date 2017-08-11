fb3233db7bfc404e54343edab7cf578499c5a10c
liwei <weili@thoughtworks.com>
2017-08-11 18:22:46 +0800

    finish FileIO

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE NoImplicitPrelude #-}
+ {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RebindableSyntax #-}
@@ -79,8 +79,9 @@ the contents of c
 -- /Tip:/ use @getArgs@ and @run@
 main ::
   IO ()
-main =
-  error "todo: Course.FileIO#main"
+main = do args <- getArgs
+          sequence $ map run args
+          return ()
 
 type FilePath =
   Chars
@@ -90,8 +91,7 @@ type FilePath =
 run ::
   FilePath
   -> IO ()
-run =
-  error "todo: Course.FileIO#run"
+run fp = getFile fp >>= getFiles . lines . snd >>= printFiles
 
 -- Given a list of file names, return list of (file name and file contents).
 -- Use @getFile@.
@@ -99,23 +99,22 @@ getFiles ::
   List FilePath
   -> IO (List (FilePath, Chars))
 getFiles =
-  error "todo: Course.FileIO#getFiles"
+  sequence . map getFile
 
 -- Given a file name, return (file name and file contents).
 -- Use @readFile@.
 getFile ::
   FilePath
   -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
+getFile path = (flip (,) $ path) <$> readFile path
 
 -- Given a list of (file name and file contents), print each.
 -- Use @printFile@.
 printFiles ::
   List (FilePath, Chars)
   -> IO ()
-printFiles =
-  error "todo: Course.FileIO#printFiles"
+printFiles fs =
+  (sequence $ map (\(f,c) -> printFile f c) fs) >> return ()
 
 -- Given the file name, and file contents, print them.
 -- Use @putStrLn@.
@@ -123,5 +122,5 @@ printFile ::
   FilePath
   -> Chars
   -> IO ()
-printFile =
-  error "todo: Course.FileIO#printFile"
+printFile f c = do putStrLn $ "============" ++ f
+                   putStrLn c
