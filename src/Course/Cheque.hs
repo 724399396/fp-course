2d4c0730549b1d75741e1a641ee9208ccf72ae14
liwei <weili@thoughtworks.com>
2017-09-06 18:34:23 +0800

    start cheque

--- src/Course/Cheque.hs
+++ src/Course/Cheque.hs
@@ -10,7 +10,7 @@ It will accept a numeric value as input, representing an amount of money, and co
 For example, the input "1.11" will result in a return value of "one dollar and eleven cents"
 
 Invalid characters should be ignored, meaning that every input string has an output string.
-The empty string produces "zero dollars and zero cents"
+nThe empty string produces "zero dollars and zero cents"
 
 There is a `test` function below that lists more examples of input and output. There are also functions and
 data structures that may assist you in deriving the result. It is not compulsory that they are used.
@@ -320,8 +320,54 @@ fromChar _ =
 --
 -- >>> dollars "456789123456789012345678901234567890123456789012345678901234567890.12"
 -- "four hundred and fifty-six vigintillion seven hundred and eighty-nine novemdecillion one hundred and twenty-three octodecillion four hundred and fifty-six septendecillion seven hundred and eighty-nine sexdecillion twelve quindecillion three hundred and forty-five quattuordecillion six hundred and seventy-eight tredecillion nine hundred and one duodecillion two hundred and thirty-four undecillion five hundred and sixty-seven decillion eight hundred and ninety nonillion one hundred and twenty-three octillion four hundred and fifty-six septillion seven hundred and eighty-nine sextillion twelve quintillion three hundred and forty-five quadrillion six hundred and seventy-eight trillion nine hundred and one billion two hundred and thirty-four million five hundred and sixty-seven thousand eight hundred and ninety dollars and twelve cents"
+chunksOf :: Int -> List a -> List (List a)
+chunksOf _ Nil = Nil
+chunksOf n x = (take n x) :. (chunksOf n (drop n x))
+
+strConcat x y z = y :. x :. z
+
+lookup :: (Eq a) => (a -> Bool) -> List a -> Optional a
+lookup _ Nil = Empty
+lookup f (y :. ys) = if (f y) then Full y else lookup f ys
+
+aboveTen :: Int -> Optional (List a)
+aboveTen x = let aboveTenList = listh [(10, "ten"), (11, "eleven"), (12, "twelve")]
+             in ((:. Nil) . snd) <$> lookup ((== x) . fst) aboveTenList
+
+aboveTwenty x = case x of
+                        2 -> Full "twenty"
+                        3 -> Full "thirty"
+                        4 -> Full "fourty"
+                        _ -> Empty
+
+single :: Char -> Optional Chars
+single x = showDigit <$> fromChar x
+
+noUnit :: Chars -> Optional (List Chars)
+noUnit x = do i <- read x
+              let cx = show i
+              sequence $ single <$> listh cx
+noUnit w@('1' :. _ :. Nil) = aboveTen =<< (read w)
+noUnit (x :. y :. Nil) = sequence $ (strConcat "-") <$> (aboveTwenty =<< read (x :. Nil)) <*> single y
+noUnit (x :. y :. z :. Nil) = (\h u -> case u of
+                                         "zero " -> " hundred "
+                                         _ -> " hundred and ") <$> single x <*> noUnit (y :. z :. Nil)
+noUnit _ = Full "zero"
+
 dollars ::
   Chars
   -> Chars
-dollars =
-  error "todo: Course.Cheque#dollars"
+dollars str =
+  let legalFilter = filter ((/= Empty) . fromChar)
+      (intPart, decPart) = break ('.' ==) str
+      intStr = case sequence $ noUnit <$> chunksOf 3 (legalFilter intPart) of
+                 Empty -> "zero "
+                 Full Nil -> "zero "
+                 Full x -> join $ map (uncurry $ strConcat " ") $ zip x illion
+      decStr = case noUnit $ take 2 (legalFilter decPart ++ replicate 2 '0') of
+                                       Empty -> "zero "
+                                       Full x -> x ++ " "
+      unit x = case x of
+                 "one " -> ""
+                 _ -> "s"
+  in intStr ++ "dollar" ++ (unit intStr) ++ " and " ++ decStr ++ "cent" ++ (unit decStr)
